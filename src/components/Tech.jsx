4248008cec240536f34b544a255925002d92608d
Frankam487 <fkamgang06@gmail.com>
2025-04-27 21:40:08 -0700

    people say

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 gap-5">
+    <div className="flex flex-row flex-wrap justify-center gap-10 sm:flex-col">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
