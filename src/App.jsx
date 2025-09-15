1d1cae50709d0fda8c15bfea63ddf86a3f724d65
Frankandev <fkamgang06@gmail.com>
2025-09-15 01:45:02 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,25 @@ import Footer from "./components/Footer.jsx";
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+            <Footer />
+          </div>
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        <Footer/>
-        </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 }
