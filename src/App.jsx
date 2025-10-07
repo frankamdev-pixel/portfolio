e5d98185c1c865ad7c006c7fb30112ae6cab1f28
Frankandev <fkamgang06@gmail.com>
2025-10-07 12:46:48 +0100

    ProgressBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-// import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer.jsx";
 import ScrollProgressBar from "./ScrollProgressBar.jsx";
 
 // const Works = lazy(() => import("./components/Works.jsx")); 
